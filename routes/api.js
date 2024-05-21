b6b4b54034de25ca8c1261a568043c0079af9ec7
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-05-21 18:22:08 -0500

    final final changes

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const router =require('express').Router();
 const {v4:uuidv4} =require('uuid');
 const fs = require("fs");
 
-router.get('./notes', (req, res) =>{ 
+router.get('/notes', (req, res) =>{ 
     let n = fs.readFileSync("./develop/db.json", "utf8");
     console.log(n);
     n = JSON.parse(n);
@@ -27,16 +27,16 @@ router.post('/notes', (req, res) =>{
 });
 });
 
-router.delete('./notes/:id', (req,res) => {
-    const noteId = parseInt(req.params.id);
-    fs.readFile(path.join(__dirname, './develop/db.json'), 'utf8', (err,data) => {
-        if (err) throw err;
-        let notes = JSON.parse(data);
-        notes = notes.filter(note => note.id !== noteId);
-        fs.writeFile(path.join(__dirname, './develop/db.json'), JSON.stringify(notes), err => {
-            if (err) throw err;
-            res.json({ succes: true });
-        });
-    });
-});
+// router.delete('/notes/:id', (req,res) => {
+//     const noteId = parseInt(req.params.id);
+//     fs.readFile(path.join(__dirname, './develop/db.json'), 'utf8', (err,data) => {
+//         if (err) throw err;
+//         let notes = JSON.parse(data);
+//         notes = notes.filter(note => note.id !== noteId);
+//         fs.writeFile(path.join(__dirname, './develop/db.json'), JSON.stringify(notes), err => {
+//             if (err) throw err;
+//             res.json({ succes: true });
+//         });
+//     });
+
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,7 +9,7 @@ res.sendFile(path.join(__dirname,'../public/notes.html'));
 });
 
 router.get ('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public/index.html')); 
+    res.sendFile(path.join(__dirname,'public/index.html')); 
 });
 
 module. exports = router;
